e4135c5678c3b3709c33385ec947a67104ff1043
Nick Babenko <nick.babenko@bmcmgroup.com>
2020-12-27 21:41:13 +0000

    Added auto update

--- setup.sh
+++ setup.sh
@@ -1,11 +1,25 @@
+# Install dotfiles
+if [ -d ~/dotfiles ] 
+then
+   cd ~/dotfiles
+   git pull
+else
+    git clone git@github.com:nickbabenko/dotfiles.git ~/dotfiles
+    cd ~/dotfiles
+fi
+
 # Install Homebrew
-#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-#brew doctor
-#brew update
+if [[ $(command -v brew) == "" ]]; then
+  echo "Installing homebrew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+else
+  echo "Updating homebre"
+  brew update
+fi
 
 # Cleanup files for install
 sudo rm -rf ~/.vimrc > /dev/null 2>&1
-sudo rm -rf ~/.vim/customx > /dev/null 2>&1
+sudo rm -rf ~/.vim/custom > /dev/null 2>&1
 sudo rm -rf ~/.zshrc > /dev/null 2>&1
 sudo rm -rf ~/Brewfile > /dev/null 2>&1
 
@@ -15,7 +29,7 @@ ln -sf ~/dotfiles/config/vim ~/.vim/custom
 ln -sf ~/dotfiles/config/zshrc ~/.zshrc
 ln -sf ~/dotfiles/config/Brewfile ~/Brewfile
 
-# Brewfiles
+# Install brewfiles
 cd ~
 brew bundle
 cd -
@@ -23,8 +37,11 @@ cd -
 # Setup
 source ~/.zshrc
 
-# Vim deps
+# Vim plug
 curl -fLo '~/.vim/autoload/plug.vim' --create-dirs \
     'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# Setup git hooks
+cp hooks/* .git/hooks
 
 echo "Done!"
