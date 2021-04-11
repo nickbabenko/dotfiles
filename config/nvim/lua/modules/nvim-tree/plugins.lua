98828f8de5f2c0f6de07a2ac78ffd643fb1fd253
Nick Babenko <nick.babenko@thehutgroup.com>
2021-04-11 20:08:24 +0100

    added filetree icons

--- config/nvim/lua/modules/nvim-tree/plugins.lua
+++ config/nvim/lua/modules/nvim-tree/plugins.lua
@@ -4,6 +4,6 @@ local conf = require('modules/nvim-tree/config')
 nvimtree['kyazdani42/nvim-tree.lua'] = {
   config = conf.nvim_tree,
 }
-nvimtree['ryanoasis/vim-devicons'] = {}
+nvimtree['kyazdani42/nvim-web-devicons'] = {}
 
 return nvimtree
