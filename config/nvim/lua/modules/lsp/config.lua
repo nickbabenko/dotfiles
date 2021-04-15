dd72d208ec0447a9050d914be48fe083573197d9
Nick Babenko <nick.babenko@thehutgroup.com>
2021-04-15 22:09:10 +0100

    lsp config update

--- config/nvim/lua/modules/lsp/config.lua
+++ config/nvim/lua/modules/lsp/config.lua
@@ -20,13 +20,15 @@ config.lspconfig = function()
 
   vim.api.nvim_set_keymap("n", "gD", ":lua vim.lsp.buf.declaration()<CR>", { noremap = true, silent = true })
   vim.api.nvim_set_keymap("n", "gd", ":lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap("n", "K", ":lua vim.lsp.buf.hover()<CR>", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap("n", "<C-k>", ":lua vim.lsp.buf.implementation()<CR>", { noremap = true, silent = true })
   vim.api.nvim_set_keymap("n", "gt", ":lua vim.lsp.buf.type_definition()<CR>", { noremap = true, silent = true })
   vim.api.nvim_set_keymap("n", "gr", ":Telescope lsp_references<CR>", { noremap = true, silent = true })
   vim.api.nvim_set_keymap("n", "gh", ":lua vim.lsp.buf.hover()<CR>", { noremap = true, silent = true })
   vim.api.nvim_set_keymap("n", "gi", ":lua vim.lsp.buf.implementation()<CR>", { noremap = true, silent = true })
   vim.api.nvim_set_keymap("n", "<space>rn", ":lua vim.lsp.buf.rename()<CR>", { noremap = true, silent = true })
-  vim.api.nvim_set_keymap("n", "<c-p>", ":lua vim.lsp.diagnostic.goto_prev()<CR>", { noremap = true, silent = true })
-  vim.api.nvim_set_keymap("n", "<c-n>", ":lua vim.lsp.diagnostic.goto_next()<CR>", { noremap = true, silent = true })
+  --vim.api.nvim_set_keymap("n", "<c-p>", ":lua vim.lsp.diagnostic.goto_prev()<CR>", { noremap = true, silent = true })
+  --vim.api.nvim_set_keymap("n", "<c-n>", ":lua vim.lsp.diagnostic.goto_next()<CR>", { noremap = true, silent = true })
 end
 
 config.compe = function()
